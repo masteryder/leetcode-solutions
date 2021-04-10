1a90f028dad4b4ba21e0eb346b3319b47c4292f8
Joao Ventura <joao.ventura@buzzbrothers.ch>
2021-04-11 00:33:52 +0200

    add easy parentheses problem

---
+++ 0-Easy/20-valid_parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        ArrayList<Character> openings = new ArrayList(Arrays.asList('(','{','['));
+        ArrayList<Character> closings = new ArrayList(Arrays.asList(')','}',']'));
+        
+        int count = 0;
+        Stack<Integer> lastOpenedIndex = new Stack<Integer>();
+        
+        for(char c : s.toCharArray() )
+        {
+            if(openings.contains(c)){
+                count++;
+                lastOpenedIndex.push(openings.indexOf(c));
+            }
+            else if(closings.contains(c)){
+                if (count == 0 || closings.indexOf(c) != lastOpenedIndex.pop()) return false;
+                count--;
+            }
+            else return false;
+        }
+        
+        if(count != 0 ) return false;
+        return true;
+    }
+}
